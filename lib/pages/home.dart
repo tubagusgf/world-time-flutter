7566d01d7f43e2c9180794133c8abfe00e0dcac7
Fauzy <tubagus@sawala.tech>
2021-08-01 16:34:27 +0700

    map and routes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,7 +9,31 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Home Screen')),
+      body: SafeArea(
+          child: Column(
+        children: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/location');
+            },
+            child: Row(
+              children: <Widget>[
+                Icon(
+                  Icons.edit_location,
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  width: 5.0,
+                ),
+                Text(
+                  'Edit Location',
+                  style: TextStyle(color: Colors.black),
+                )
+              ],
+            ),
+          )
+        ],
+      )),
     );
   }
 }
