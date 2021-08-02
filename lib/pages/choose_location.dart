33230cb6c1dc085ddd929fae6c951d393655abf8
Fauzy <tubagus@sawala.tech>
2021-08-02 16:27:35 +0700

    http request example with json placeholder api

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,34 +6,13 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() async {
-    // simulate network request for a username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'beeg yoshi';
-    });
-
-    // simulate network request to get a bio of a username
-    String bio = await Future.delayed(Duration(seconds: 3), () {
-      return 'gamers';
-    });
-
-    print('$username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('hey there!');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
-        title: Text('Choose a Location'),
+        title: Center(child: Text('Choose a Location')),
         centerTitle: true,
         elevation: 0.0,
       ),
