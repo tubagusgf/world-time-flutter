1d59901e12e7f0312bf2334859e32c02599e953e
Fauzy <tubagus@sawala.tech>
2021-08-02 16:11:49 +0700

    async await

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,6 +6,27 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  void getData() async {
+    // simulate network request for a username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'beeg yoshi';
+    });
+
+    // simulate network request to get a bio of a username
+    String bio = await Future.delayed(Duration(seconds: 3), () {
+      return 'gamers';
+    });
+
+    print('$username - $bio');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+    print('hey there!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
