9bc688923507ea139c8540aa37175d8342372209
Fauzy <tubagus@sawala.tech>
2021-08-03 11:21:14 +0700

    get time from world time api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,27 +8,35 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
-    Response response =
-        await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+  void getTime() async {
+    Response response = await get(
+        Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Jakarta'));
     Map data = jsonDecode(response.body);
-    setState(() {
-      text = data['title'];
-    });
-  }
+    // print(data);
 
-  String text = 'Loading...';
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    // print(datetime);
+    // print(offset);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+  }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('$text')),
+      body: Center(child: Text('loading...')),
     );
   }
 }
